736fe7454db474530c3007cba3221c86f28f5e03
John McKie <john.mckie.dev@gmail.com>
2026-03-15 17:22:07 +0000

    basic ts in place to start handling the form submit

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,55 @@
-import './style.scss'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript -- what a laugh</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    <a href="/about/about.html">
-      <button id="about" type="button">About us</button>
-    </a>
-  </div>
-`
+// all of this content needs moving out of the index.ts file, and into separate files, and then imported in here.
+// This file should just be the entry point, and should import all the other files,
+// and call any functions that need to be called on page load.
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+type formElements = {
+  interactionContainer: HTMLDivElement | null;
+  form: HTMLFormElement | null | undefined;
+  input: HTMLInputElement | null | undefined;
+  addButton: HTMLButtonElement | null | undefined;
+  toDoList: HTMLUListElement | null;
+  doneList: HTMLUListElement | null;
+};
+
+// get this in a function, in a new file, and call that function on pageload
+const getFormElements = (): formElements => {
+  const interactionContainer = document.querySelector<HTMLDivElement>(
+    ".interaction-container",
+  );
+  const form = interactionContainer?.querySelector<HTMLFormElement>("form");
+  const input = form?.querySelector<HTMLInputElement>('input[type="text"]');
+  const addButton = form?.querySelector<HTMLButtonElement>("button");
+  const toDoList = document.querySelector<HTMLUListElement>(".to-do-list");
+  const doneList = document.querySelector<HTMLUListElement>(".done-list");
+
+  return {
+    interactionContainer,
+    form,
+    input,
+    addButton,
+    toDoList,
+    doneList,
+  };
+};
+
+const addFormSubmitListener = (
+  form: HTMLFormElement | null | undefined,
+  input: HTMLInputElement | null | undefined,
+) => {
+  form?.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("Form submitted with input:", input?.value);
+    console.log("test complete");
+  });
+};
+
+const setupTodoForm = () => {
+  const { form, input, addButton, toDoList, doneList } = getFormElements();
+  if (form && input) {
+    addFormSubmitListener(form, input);
+  }
+};
+
+setupTodoForm();
